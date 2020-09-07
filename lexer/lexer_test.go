53809064d0d7969748044576ac46778ccc885ecb
lczm <zemingchua@gmail.com>
2020-09-08 00:40:38 +0800

    Add multi token scans

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/lczm/as/token"
 )
 
-func TestIndividualScan(t *testing.T) {
+func TestIndividualTokenScan(t *testing.T) {
 	tests := []struct {
 		input           string
 		expectedType    token.TokenType
@@ -45,12 +45,53 @@ func TestIndividualScan(t *testing.T) {
 		tokens := lexer.Scan(test.input)
 		for _, token := range tokens {
 			if token.Type != tests[i].expectedType {
-				t.Fatalf("[%d] - Wrong TokenType, expected=%q, got=%q",
+				t.Fatalf("Test : [%d] - Wrong TokenType, expected=%q, got=%q",
 					i, tests[i].expectedType, token.Type)
 			}
 			if token.Literal != tests[i].expectedLiteral {
-				t.Fatalf("[%d] - Wrong Literal, expected=%q, got=%q",
+				t.Fatalf("Test : [%d] - Wrong Literal, expected=%q, got=%q",
 					i, tests[i].expectedLiteral, token.Literal)
+			}
+		}
+	}
+}
+
+func TestMultipleTokenScan(t *testing.T) {
+	tests := []struct {
+		input            string
+		expectedTypes    []token.TokenType
+		expectedLiterals []string
+	}{
+		{
+			`= + - ! * /`,
+			[]token.TokenType{token.ASSIGN, token.PLUS, token.MINUS, token.BANG, token.ASTERISK, token.SLASH},
+			[]string{"=", "+", "-", "!", "*", "/"},
+		},
+	}
+
+	lexer := New()
+	for i, test := range tests {
+		tokens := lexer.Scan(test.input)
+
+		if len(test.expectedTypes) != len(test.expectedLiterals) {
+			t.Fatalf("Test : [%d] - Mismatch amount of types and literals, expectedTypes=%d, expectedLiterals=%d",
+				i, len(test.expectedTypes), len(test.expectedLiterals))
+		}
+
+		if len(tokens) != len(test.expectedTypes) {
+			t.Fatalf("Test : [%d] - Mismatch amount of scanned tokens and expectedTokens,"+
+				"scanned=%d, expectedTokens=%d",
+				i, len(tokens), len(test.expectedTypes))
+		}
+
+		for j, token := range tokens {
+			if token.Type != tests[i].expectedTypes[j] {
+				t.Fatalf("Test : [%d - %d] - Wrong TokenType, expected=%q, got=%q",
+					i, j, tests[i].expectedTypes[j], token.Type)
+			}
+			if token.Literal != tests[i].expectedLiterals[j] {
+				t.Fatalf("Test : [%d - %d] - Wrong Literal, expected=%q, got=%q",
+					i, j, tests[i].expectedLiterals[j], token.Literal)
 			}
 		}
 	}
