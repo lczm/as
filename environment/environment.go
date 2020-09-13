bf5b0275db96a4f793dd6e7aa01439829e27e92e
lczm <zemingchua@gmail.com>
2020-09-14 01:55:49 +0800

    Add some helper functions to an environment
    This does not account for nested environments right now,
    this assumes that all environments are global

--- environment/environment.go
+++ environment/environment.go
@@ -1,6 +1,18 @@
 package environment
 
-type Environment struct{}
+import "github.com/lczm/as/object"
+
+type Environment struct {
+	Values map[string]object.Object
+}
+
+func (e *Environment) Add(name string, value object.Object) {
+	e.Values[name] = value
+}
+
+func (e *Environment) Get(name string) object.Object {
+	return e.Values[name]
+}
 
 func New() *Environment {
 	e := &Environment{}
