c365d74c130337f79b490ce4cb24136ac467f03a
lczm <zemingchua@gmail.com>
2020-10-10 14:20:41 +0800

    Add test for append function

--- tests/builtin_test.go
+++ tests/builtin_test.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/lczm/as/interpreter"
@@ -82,6 +83,58 @@ func TestTypeFunc(t *testing.T) {
 		if interpreter.Environment.Exists(outputVariable) {
 			obj := interpreter.Environment.Get(outputVariable)
 			if obj.String() != test.expectedOutput {
+				t.Fatalf("Test: [%d] - Incorrect value, expected=%s, got=%s",
+					i, test.expectedOutput, obj.String())
+			}
+		}
+	}
+}
+
+func TestAppendFunc(t *testing.T) {
+	tests := []struct {
+		input          string
+		expectedOutput string
+	}{
+		{
+			`var output = [];`,
+			"[]",
+		},
+		{
+			`
+            var output = [];
+            output = append(output, 1);
+            output = append(output, 2);
+            `,
+			"[1 2]",
+		},
+		{
+			`
+            var output = [];
+            for (var a = 0; a < 5; a++) {
+                output = append(output, a);
+            }
+            `,
+			"[0 1 2 3 4]",
+		},
+	}
+
+	outputVariable := "output"
+	lexer := lexer.New()
+
+	for i, test := range tests {
+		tokens := lexer.Scan(test.input)
+		parser := parser.New(tokens)
+		statements := parser.Parse()
+
+		interpreter := interpreter.New(statements)
+		interpreter.Start()
+
+		// Directly hook into the environment to check for output variable.
+		if interpreter.Environment.Exists(outputVariable) {
+			obj := interpreter.Environment.Get(outputVariable)
+			// The object returns with strings, this should be fixed in the future, but
+			// for the time being this should do well enough for tests
+			if strings.TrimSuffix(obj.String(), "\n") != test.expectedOutput {
 				t.Fatalf("Test: [%d] - Incorrect value, expected=%s, got=%s",
 					i, test.expectedOutput, obj.String())
 			}
