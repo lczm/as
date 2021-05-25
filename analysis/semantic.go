c9b2dad10a2073124e8265aab4bf665e927376ba
lczm <zemingchua@gmail.com>
2021-05-25 09:55:58 +0800

    Refactor semantic analyzer to use a stack to check for nests

--- analysis/semantic.go
+++ analysis/semantic.go
@@ -7,7 +7,8 @@ import (
 )
 
 type SemanticAnalyzer struct {
-	variables  map[string]bool
+	// Array of variable mapping (Stack)
+	variables  []map[string]bool
 	statements []ast.Statement
 }
 
@@ -21,20 +22,64 @@ func (s *SemanticAnalyzer) Analyze() {
 func (s *SemanticAnalyzer) Eval(astNode ast.AstNode) {
 	switch node := astNode.(type) {
 	case *ast.VariableStatement:
-		_, found := s.variables[node.Name.Literal]
+		_ = s.resolveVariable(node)
+	}
+}
+
+// Resolves a variable into the scope
+func (s *SemanticAnalyzer) resolveVariable(node *ast.VariableStatement) bool {
+	var scope *map[string]bool
+	for i := 0; i < len(s.variables)-1; i++ {
+		scope = s.getScope(len(s.variables) - i - 1)
+		_, found := (*scope)[node.Name.Literal]
 		if found {
-			// Shadow warning
+			// Shadow warning, variable name has been declared already but now it is declared again
 			fmt.Printf("Shadow warning at line %d, Declaring an already declared variable: \"%s\"", node.Name.Line, node.Name.Literal)
-		} else {
-			s.variables[node.Name.Literal] = true
+			return false
 		}
 	}
+	(*scope)[node.Name.Literal] = true
+	return true
+}
+
+func (s *SemanticAnalyzer) pushScope() {
+	s.variables = append(s.variables, make(map[string]bool))
+}
+
+func (s *SemanticAnalyzer) popScope() {
+	// There should always be one scope, the global scope
+	// So if there are exists more than one scope; then it is
+	// safe to pop the scope out
+	if len(s.variables) > 1 {
+		s.variables = s.variables[:len(s.variables)-1]
+	}
+}
+
+func (s *SemanticAnalyzer) getScope(index int) *map[string]bool {
+	// Check if out of bounds
+	if index < 0 || index > len(s.variables)-1 {
+		panic("Tried to get a scope that is out of bounds")
+	}
+	return &s.variables[index]
+}
+
+func (s *SemanticAnalyzer) getLastScope() *map[string]bool {
+	// This can include the global scope, unlike popScope()
+	if len(s.variables) >= 1 {
+		s.variables = s.variables[:len(s.variables)-1]
+		return &s.variables[len(s.variables)-1]
+	}
+	// If this happens, something bugged out in the analyzer
+	// just panic out of this.
+	panic("Analyzer tried to get last scope, but there are no scopes remaining.")
 }
 
 func New(statements []ast.Statement) *SemanticAnalyzer {
 	s := &SemanticAnalyzer{
-		variables:  make(map[string]bool),
+		variables:  make([]map[string]bool, 1),
 		statements: statements,
 	}
+	// Create global scope
+	s.variables = append(s.variables, make(map[string]bool))
 	return s
 }
